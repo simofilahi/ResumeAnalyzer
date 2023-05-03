d2c17a358adae2636454bd3d8aa0639aa8b3ccb8
mfilahi <mfilahi@gear9.ma>
2023-05-03 10:29:18 +0100

    favicon and title added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>TalentScan</title>
+      </Head>
       <body>
         <script
           data-name="BMC-Widget"
